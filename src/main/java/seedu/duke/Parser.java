b7cee8f38c7aef81aecf4ce2acc3661232d88bcc
Wesley <league4orce@gmail.com>
2026-03-08 00:51:55 +0800

    Add new line and modify indent for parser

--- src/main/java/seedu/duke/Parser.java
+++ src/main/java/seedu/duke/Parser.java
@@ -12,22 +12,23 @@ public class Parser {
      * which command is being called
      * then returns the class corresponding
      * with the command with the correct fields
+     *
      * @param inputText the string from the user input
      * @return the class associated with the command
-     *          that was parsed from the input text
+     * that was parsed from the input text
      */
     public Command parse(String inputText) {
         CommandWord commandWord = parseCommand(inputText);
         return switch (commandWord) {
-            case LIST -> new ListCommand();
-            case HELP -> new HelpCommand();
-            case CREATE -> new CreateCommand();
-            case BALANCE -> new BalanceCommand();
-            case VALIDATE -> new ValidateCommand();
-            case VIEWBLOCK -> new ViewBlockCommand();
-            case EXIT -> new ExitCommand();
-            case SEND -> new SendCommand();
-            case KEYGEN -> new KeygenCommand();
+        case LIST -> new ListCommand();
+        case HELP -> new HelpCommand();
+        case CREATE -> new CreateCommand();
+        case BALANCE -> new BalanceCommand();
+        case VALIDATE -> new ValidateCommand();
+        case VIEWBLOCK -> new ViewBlockCommand();
+        case EXIT -> new ExitCommand();
+        case SEND -> new SendCommand();
+        case KEYGEN -> new KeygenCommand();
         };
     }
 }
