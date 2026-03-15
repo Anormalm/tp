73e4c3e2122696f32d7af1d0b97c4e9a8f236ddc
Anormalm <e1480030@u.nus.edu>
2026-03-16 03:23:00 +0800

    Add backward-compatible execute overload for tests

--- src/main/java/seedu/duke/command/Command.java
+++ src/main/java/seedu/duke/command/Command.java
@@ -6,6 +6,9 @@ import seedu.duke.model.Blockchain;
 public abstract class Command {
     protected String helpDescription;
     public abstract void execute(String description, Blockchain blockchain) throws Exceptions;
+    public void execute(Blockchain blockchain) throws Exceptions {
+        execute("", blockchain);
+    }
 
     Command(String helpDescription) {
         this.helpDescription = helpDescription;
