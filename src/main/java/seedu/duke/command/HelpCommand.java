dcc3a73612e0477f06b7ae9fbce084b3d93e4dce
Wesley <league4orce@gmail.com>
2026-03-27 14:05:27 +0800

    Create tutorial method

--- src/main/java/seedu/duke/command/HelpCommand.java
+++ src/main/java/seedu/duke/command/HelpCommand.java
@@ -4,6 +4,8 @@ import seedu.duke.Parser;
 import seedu.duke.model.Blockchain;
 import seedu.duke.model.WalletManager;
 
+import java.util.Scanner;
+
 public class HelpCommand extends Command {
     private static final String HELP_DESCRIPTION = """
             Format: help [c/COMMAND]
@@ -13,6 +15,7 @@ public class HelpCommand extends Command {
             If no valid COMMAND is given: lists all the available commands
             If a valid COMMAND is given: displays details regarding that command
             """;
+
     private static final String HELP_MESSAGE =
             "For more details about each command type 'help c/COMMAND', eg. 'help c/list'";
 
@@ -45,6 +48,33 @@ public class HelpCommand extends Command {
             }
         } catch (IllegalArgumentException e) {
             System.out.println("Please input a valid command, use 'help' to see the list of commands");
+        }
+    }
+
+    private void tutorial() {
+        Scanner in = new Scanner(System.in);
+        String[] instructions = {
+                "create w/alice",
+                "create w/bob",
+                "keygen w/alice",
+                "keygen w/bob",
+                "list",
+        };
+        int index = 0;
+
+        while (true) {
+            System.out.println("Enter the following command:");
+            System.out.println(instructions[index]);
+            String input = in.nextLine().strip();
+            if (input.equals(instructions[index])) {
+                // Do the executing
+                index++;
+            } else if (input.equals("exit()")) {
+                return;
+            } else {
+                System.out.println("That was not the given instruction");
+                System.out.println("If you wish to exit type: exit()");
+            }
         }
     }
 }
